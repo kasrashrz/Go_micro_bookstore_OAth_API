4e0d667f5f9b0c6b3b64a351653e45ca457465ae
kasra shirazi <kasra.shirazi.1234@gmail.com>
2022-03-24 07:52:07 +0430

    adding rest client to send requst

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/gocql/gocql v1.0.0
+	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
---
+++ src/repository/rest/users_repository.go
@@ -0,0 +1,50 @@
+package rest
+
+import (
+	"encoding/json"
+	"github.com/kasrashrz/Go_micro_bookstore_OAth_API/src/domain/user"
+	"github.com/kasrashrz/Go_micro_bookstore_OAth_API/src/utils/errors"
+	"github.com/mercadolibre/golang-restclient/rest"
+	"time"
+)
+
+var (
+	usersRestClient = rest.RequestBuilder{
+		BaseURL: "https://api.bookstore.com",
+		Timeout: 100 * time.Millisecond,
+	}
+)
+
+type RestUserRepository interface {
+	LoginUser(string, string) (*user.User, *errors.RestErr)
+}
+
+type usersRepository struct{}
+
+func newUserRepository() RestUserRepository {
+	return &usersRepository{}
+}
+
+func (repository *usersRepository) LoginUser(email string, password string) (*user.User, *errors.RestErr) {
+	request := user.UserLoginRequest{
+		Email:    email,
+		Password: password,
+	}
+	response := usersRestClient.Post("/users/login", request)
+	if response == nil || response.Response == nil {
+		return nil, errors.InternalServerError("invalid rest client response when trying to login user")
+	}
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		err := json.Unmarshal(response.Bytes(), &restErr)
+		if err != nil {
+			return nil, errors.InternalServerError("invalid error interface when trying to login user")
+		}
+		return nil, &restErr
+	}
+	var user user.User
+	if err := json.Unmarshal(response.Bytes(), &user); err != nil {
+		return nil, errors.InternalServerError("error when trying to unmarshal users response")
+	}
+	return &user, nil
+}
